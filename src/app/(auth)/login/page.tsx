e69ad72a48f831b6641ac629935717d4b6a81c99
yusufswe <muhdysf89@gmail.com>
2024-07-22 02:00:06 +0700

    add show password toggle

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,12 +2,12 @@
 
 import loginImage from "@/assets/loginImage.png";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
@@ -18,188 +18,132 @@ import Image from "next/image";
 import { redirect } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function Login() {
-    const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
+  const [showPassword, setShowPassword] = useState(false);
 
-    if (status == "authenticated") {
-        redirect("/admin/umkm");
-    }
+  if (status == "authenticated") {
+    redirect("/admin/umkm");
+  }
 
-    const form = useForm<LoginPayload>({
-        resolver: zodResolver(loginPayloadSchema),
-        defaultValues: {
-            username: "",
-            password: "",
+  const form = useForm<LoginPayload>({
+    resolver: zodResolver(loginPayloadSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit() {
+    toast.promise(
+      signIn("credentials", {
+        username: form.getValues("username"),
+        password: form.getValues("password"),
+        redirect: false,
+      }),
+      {
+        loading: "Loading...",
+        success: (data) => {
+          form.reset();
+          toast.success("Berhasil masuk");
+          return "Berhasil masuk";
         },
-    });
-
-    function onSubmit() {
-        // signIn("credentials", {
-        //     username: form.getValues("username"),
-        //     password: form.getValues("password"),
-        // })
-        //     .then((res: any) => {
-        //         if (res.error) {
-        //             toast.error("Gagal masuk");
-        //             return;
-        //         }
-        //         form.reset();
-        //         toast.success("Berhasil masuk");
-        //     })
-        //     .catch((err) => {
-        //         if (err.error) {
-        //             toast.error("Gagal masuk");
-        //             form.setError("username", {
-        //                 type: "manual",
-        //                 message: "Username atau password salah",
-        //             });
-        //             form.setError("password", {
-        //                 type: "manual",
-        //                 message: "Username atau password salah",
-        //             });
-        //             return;
-        //         }
-        //     });
-
-        toast.promise(
-            signIn("credentials", {
-                username: form.getValues("username"),
-                password: form.getValues("password"),
-                redirect: false
-            }),
-            {
-                loading: "Loading...",
-                success: (data) => {
-                    form.reset();
-                    toast.success("Berhasil masuk");
-                    return "Berhasil masuk";
-                },
-                error: (err) => {
-                    form.setError("username", {
-                        type: "manual",
-                        message: "Username atau password salah",
-                    });
-                    form.setError("password", {
-                        type: "manual",
-                        message: "Username atau password salah",
-                    });
-                    return "Gagal masuk";
-                },
-            }
-        );
-    }
-
-    return (
-        <div className="flex items-center justify-center min-h-screen ">
-            <div className=" w-full max-w-md p-8">
-                <Image
-                    src={loginImage}
-                    width={300}
-                    height={300}
-                    alt="login image"
-                    className="mx-auto object-cover"
-                />
-                <h2 className="mb-6 text-3xl font-semibold  text-gray-700">
-                    Masuk
-                </h2>
-                <p className="mb-6 text-sm  text-green-prim">Masuk ke Akun</p>
-
-                <Form {...form}>
-                    <form
-                        // method="POST"
-                        className="space-y-4"
-                        onSubmit={form.handleSubmit(onSubmit)}
-                    >
-                        <FormField
-                            control={form.control}
-                            name="username"
-                            render={({ field, fieldState }) => (
-                                <FormItem>
-                                    <FormLabel>Nama Pengguna</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            {...field}
-                                            placeholder="Nama Pengguna"
-                                            className={cn(
-                                                fieldState.error &&
-                                                    "border-red-400"
-                                            )}
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="password"
-                            render={({ field, fieldState }) => (
-                                <FormItem>
-                                    <FormLabel>Kata Sandi</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            {...field}
-                                            type="password"
-                                            placeholder="••••••••"
-                                            className={cn(
-                                                fieldState.error &&
-                                                    "border-red-400"
-                                            )}
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-
-                        {/* <div className="mb-4">
-                            <label
-                                htmlFor="nama"
-                                className="block mb-2 text-sm font-medium "
-                            >
-                                Nama Pengguna
-                            </label>
-                            <input
-                                type="text"
-                                id="nama"
-                                className="w-full px-3 py-2 border rounded-lg focus:outline-none"
-                                placeholder="Nama pengguna"
-                                required
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <label
-                                htmlFor="password"
-                                className="block mb-2 text-sm font-medium text-gray-600"
-                            >
-                                Kata Sandi
-                            </label>
-                            <input
-                                type="password"
-                                id="password"
-                                className="w-full px-3 py-2  border rounded-lg focus:outline-none"
-                                placeholder="••••••••"
-                                required
-                            />
-                        </div> */}
-                        <button
-                            // disabled={form.formState.isSubmitting}
-                            type="submit"
-                            className="w-full px-4 py-2 text-white bg-green-btn rounded-lg focus:outline-none hover:bg-green-btn-hover transition-colors"
-                        >
-                            Masuk
-                        </button>
-                        {/* <button
-                            onClick={() => signIn()}
-                            type="button"
-                            className="w-full px-4 py-2 text-white bg-green-btn rounded-lg focus:outline-none"
-                        >
-                            Masuk
-                        </button> */}
-                    </form>
-                </Form>
-            </div>
-        </div>
+        error: (err) => {
+          form.setError("username", {
+            type: "manual",
+            message: "Username atau password salah",
+          });
+          form.setError("password", {
+            type: "manual",
+            message: "Username atau password salah",
+          });
+          return "Gagal masuk";
+        },
+      }
     );
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className=" w-full max-w-md p-8">
+        <Image
+          src={loginImage}
+          width={300}
+          height={300}
+          alt="login image"
+          className="mx-auto object-cover"
+        />
+        <h2 className="mb-6 text-3xl font-semibold  text-gray-700">Masuk</h2>
+        <p className="mb-6 text-sm  text-green-prim">Masuk ke Akun</p>
+
+        <Form {...form}>
+          <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field, fieldState }) => (
+                <FormItem>
+                  <FormLabel>Nama Pengguna</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Nama Pengguna"
+                      className={cn(fieldState.error && "border-red-400")}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field, fieldState }) => (
+                <FormItem>
+                  <FormLabel>Kata Sandi</FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Input
+                        {...field}
+                        type={showPassword ? "text" : "password"}
+                        placeholder="••••••••"
+                        className={cn(
+                          fieldState.error && "border-red-400",
+                          "pr-10"
+                        )}
+                      />
+                      <Button
+                        type="button"
+                        variant="ghost"
+                        size="icon"
+                        className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? (
+                          <EyeOff className="h-4 w-4 text-gray-500" />
+                        ) : (
+                          <Eye className="h-4 w-4 text-gray-500" />
+                        )}
+                      </Button>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-white bg-green-btn rounded-lg focus:outline-none hover:bg-green-btn-hover transition-colors"
+            >
+              Masuk
+            </button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -19,7 +19,8 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-indigo-600 underline-offset-4 hover:underline",
         info: "bg-blue-500 text-white hover:bg-blue-600",
-        primary: "rounded-md bg-green-btn px-4 py-2 text-sm text-white transition-colors hover:bg-green-btn-hover"
+        primary:
+          "rounded-md bg-green-btn px-4 py-2 text-sm text-white transition-colors hover:bg-green-btn-hover",
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -33,26 +34,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
